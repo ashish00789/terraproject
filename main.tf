4b9d3226e8500a81baaeb74631513b714b338861
ashish00789 <118556183+ashish00789@users.noreply.github.com>
2023-01-05 22:32:36 +0530

    terraform commits

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAQFGQMYG7GUO4DYMU"
+secret_key = "UmAi8gz4ajFW9bn1IpEtRJQY3cLgADoLKlIBhyk3"
+}
+
+resource "aws_instance" "ec2_example" {
+  ami                    = "ami-0cca134ec43cf708f"
+  instance_type          = var.instance_type
+  tags = {
+    Name = "Terraform EC2"
+  }
+}
